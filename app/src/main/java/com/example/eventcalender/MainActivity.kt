711b018ba7b5802a4d1468e9162250ec88ef84fd
Jessie <jessietajalle08@gmail.com>
2024-11-20 14:25:34 -0800

    Added buttons to add/edit/delete

--- app/src/main/java/com/example/eventcalender/MainActivity.kt
+++ app/src/main/java/com/example/eventcalender/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.example.eventcalender
 
 import android.os.Bundle
+import android.widget.CalendarView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
