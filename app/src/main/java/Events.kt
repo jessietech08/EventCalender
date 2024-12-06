cafe0e6522e214d474e2bc3b407ad0792efcb44f
Jessie <jessietajalle08@gmail.com>
2024-12-05 18:19:16 -0800

    Created Event class to keep track of the id, event and the event date

---
+++ app/src/main/java/Events.kt
@@ -0,0 +1,19 @@
+package com.example.eventcalender
+
+import android.widget.EditText
+
+class Events {
+    var id: Int = 0
+    var eventTitle: String? = null
+    var eventDate: String? = null
+
+    constructor(id: Int, eventTitle: String,  eventDate: String) {
+        this.id = id
+        this.eventTitle = eventTitle
+        this.eventDate = eventDate
+    }
+    constructor(eventTitle: String, eventDate: String) {
+        this.eventTitle = eventTitle
+        this.eventDate = eventDate
+    }
+}
